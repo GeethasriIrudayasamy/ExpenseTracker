15cbaba4533e23e4054f50ba110a9ee507fcfe8b
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-11-30 20:41:43 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
 import SignUp from "./Components/Pages/SignUp";
+import Profile from "./Components/Profile";
 
 const App = () => {
     return (
-        <div>
-            <SignUp />
-        </div>
+        <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
     );
 };
 
